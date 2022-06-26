e92715cd77f11b67b85e4500156e1ffcfdad3778
Amy Householder <96360869+anhouseh@users.noreply.github.com>
2022-06-26 13:42:16 -0700

    Add files via upload

--- GuessTheWordscript.js
+++ GuessTheWordscript.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const letterGuess = document.querySelector(".letter-guess");
 const progressWord = document.querySelector(".progress-word");
@@ -8,6 +8,8 @@ const message = document.querySelector(".message")
 const playAgainButton = document.querySelector("play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
+
 
 const placeholder = function (word) {
     const placeholderLetters = [];
@@ -22,7 +24,36 @@ placeholder(word);
 
 guessButton.addEventListener("click", function (e) {
     e.preventDefault ();
+    message.innerText = "";
     const valueInput = letterInput.value;
-    console.log(valueInput);
-    letterInput.value = "";
-)};
+    const goodguess = validateInput(guess);
+
+    if (goodGuess) {
+        makeGuess(guess);
+
+    }
+    letterinput.value ="";
+});
+
+const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/ 
+    if (input.length ===  0) = {
+        message.innerText = "Please enter a letter.";
+    } else if (input.length > 1) {
+        message.innerText = "Please enter a single letter.";
+    } else if (!input.match(acceptedLetter)) {
+        message.innertext = "Please enter a letter from A to Z.";
+    } else {
+        return input;
+    }
+ };
+
+ const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        message.innerText = "You already guessed that leter, silly. Try again";
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+ };
